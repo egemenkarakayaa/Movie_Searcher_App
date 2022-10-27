1e7d0e8b90b4c3ccee09e22a37fe72be6b4a5b35
egemenkarakayaa <egemenkarakayaa@gmail.com>
2022-10-27 15:24:27 +0300

    Some necessary components and tableview functions added.

--- Movie_Search_App/ViewController.swift
+++ Movie_Search_App/ViewController.swift
@@ -7,16 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet var field: UITextField!
+    
+    var movies = [Movie]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        table.delegate = self
+        table.dataSource = self
+        field.delegate = self
         
     }
 
     
+    // Field
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchMovies()
+        return true
+    }
+    
+    func searchMovies() {
+        
+        field.resignFirstResponder()
+        
+        
+    }
+    
+    
+    // Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Show movie details
+        
+        
+    }
+    
     
 
+}
+
+struct Movie {
+    
+    
+    
 }
