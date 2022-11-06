7f016a438969d2c5a7a8cf57774fcff1ddecea10
egemenkarakayaa <egemenkarakayaa@gmail.com>
2022-11-06 18:11:15 +0300

    Custom cell added and configured.

---
+++ Movie_Search_App/MovieTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  MovieTableViewCell.swift
+//  Movie_Search_App
+//
+//  Created by Egemen Karakaya on 5.11.2022.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var movieYearLabel: UILabel!
+    @IBOutlet var moviePosterImageView: UIImageView!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    static let identifier = "MovieTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "MovieTableViewCell",
+                     bundle: nil)
+    }
+    
+    
+    func configure(with model: Movie) {
+        
+        self.movieTitleLabel.text = model.Title
+        self.movieYearLabel.text = model.Year
+        let url = model.Poster
+        let data = try! Data(contentsOf: URL(string: url)!)
+        self.moviePosterImageView.image = UIImage(data: data)
+
+        
+    }
+    
+}
